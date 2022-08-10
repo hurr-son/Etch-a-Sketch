e7fbd46c17f072845914cfee7024de90ad8fcaeb
hurr-son <hprewett05@gmail.com>
2022-08-10 15:35:19 -0500

    The create grid function now resizes whilst keeping width

--- script.js
+++ script.js
@@ -5,18 +5,20 @@ const divContainer = document.getElementById('divContainer');
 
 //Using javascript, create 256 (16 x 16) square divs
 
-function createDivsForGrid(rows, columns) {
-    for (i = 0 ;i < rows; i++) {
+function createDivsForGrid(size) {
+    let gridSize = (480 / size + 'px').toString()
+    console.log(gridSize)
+    for (i = 0 ;i < size; i++) {
         const row = document.createElement('div');
         row.classList.add('gridRow');
         row.style.display = 'flex';
-        for (j = 0 ;j < columns; j++) {
+        for (j = 0 ;j < size; j++) {
             const gridDiv =  document.createElement('div');
-            gridDiv.style.width = '20px';
-            gridDiv.style.height = '20px';
+            gridDiv.style.width = gridSize
+            gridDiv.style.height = gridSize
             gridDiv.style.border = '1px solid black';
             gridDiv.classList.add('gridDiv');
-            gridDiv.style.display = 'flex';
+            
             
             row.appendChild(gridDiv);
             
@@ -26,7 +28,7 @@ function createDivsForGrid(rows, columns) {
 
 }
 
-createDivsForGrid(16, 16);
+createDivsForGrid(16);
 
 
 //Figure out a way to use the 'hover' event to make the 
