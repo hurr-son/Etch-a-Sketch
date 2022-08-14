b730d1bee12e51a842b328b8429f7056a1eceb4e
hurr-son <hprewett05@gmail.com>
2022-08-14 10:57:42 -0500

    Found a solution to the grid sizing problem

--- script.js
+++ script.js
@@ -10,20 +10,26 @@ const divContainer = document.getElementById('divContainer');
 
 function createDivsForGrid(size) {
     
-    let gridSize = (480 / size + 'px').toString()
+    divContainer.style.maxWidth = '100%';
+    
+    
+    
+    let gridSize = (((480 / size) -2) + 'px').toString()
     for (i = 0 ;i < size; i++) {
-        const row = document.createElement('div');
+        let row = document.createElement('div');
         row.classList.add('gridRow');
         row.style.display = 'flex';
         row.style.flex = 'auto';
         for (j = 0 ;j < size; j++) {
-            const gridDiv =  document.createElement('div');
+            let gridDiv =  document.createElement('div');
             gridDiv.style.width = gridSize;
             gridDiv.style.height = gridSize;
             
-            gridDiv.style.border = '1px solid black';
+            gridDiv.style.border = '1px solid grey';
             gridDiv.classList.add('gridDiv');
             gridDiv.style.display = 'flex';
+            gridDiv.style.flex = 'auto';
+            gridDiv.style.backgroundColor = 'white';
             
             
             row.appendChild(gridDiv);
@@ -41,7 +47,7 @@ function createDivsForGrid(size) {
     
     allBoxes.forEach(div => {div.addEventListener('mouseover', 
     function handleMouseOver() {
-        div.style.backgroundColor = 'red';
+        div.style.backgroundColor = 'rgb(15, 61, 62)';
     })})
 
     //This function clears the sketch with the refresh
@@ -61,7 +67,7 @@ const loadButton = document.getElementById('load');
 const clearButton = document.getElementById('clear')
 gridValue.innerHTML = gridValue.value;
 
-createDivsForGrid(16)
+createDivsForGrid(10)
 
 function removeChildNodes(parent) {
     while(parent.firstChild) {
