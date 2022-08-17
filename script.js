305677cec1f8b540075b344182a69e5cba61fa0a
hurr-son <108298072+hurr-son@users.noreply.github.com>
2022-08-16 19:45:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,13 +6,10 @@ const divContainer = document.getElementById('divContainer');
 
 
 
-//Using javascript, create 256 (16 x 16) square divs
 
 function createDivsForGrid(size) {
     
     divContainer.style.maxWidth = '100%';
-    
-    
     
     let gridSize = (((480 / size) -2) + 'px').toString()
     for (i = 0 ;i < size; i++) {
@@ -23,34 +20,25 @@ function createDivsForGrid(size) {
         for (j = 0 ;j < size; j++) {
             let gridDiv =  document.createElement('div');
             gridDiv.style.width = gridSize;
-            gridDiv.style.height = gridSize;
-            
+            gridDiv.style.height = gridSize;          
             gridDiv.style.border = '1px solid grey';
             gridDiv.classList.add('gridDiv');
             gridDiv.style.display = 'flex';
             gridDiv.style.flex = 'auto';
-            gridDiv.style.backgroundColor = 'white';
-            
+            gridDiv.style.backgroundColor = 'white';    
             
             row.appendChild(gridDiv);
             
         }
         divContainer.appendChild(row);
     }
-    
-    //Figure out a way to use the 'hover' event to make the 
-    //divs change color as the mouse hovers over them 
-    
+   
     let allBoxes = document.querySelectorAll('.gridDiv');
-
-    
     
     allBoxes.forEach(div => {div.addEventListener('mouseover', 
     function handleMouseOver() {
         div.style.backgroundColor = 'rgb(15, 61, 62)';
     })})
-
-    //This function clears the sketch with the refresh
     
     function refresh() {
     allBoxes.forEach((div) => {div.style.backgroundColor = 'white'});
@@ -60,8 +48,6 @@ clearButton.addEventListener('click', refresh)
 
 }
 
-
-//Handling user input
 const gridValue = document.getElementById('gridValue');
 const loadButton = document.getElementById('load');
 const clearButton = document.getElementById('clear')
@@ -75,17 +61,12 @@ function removeChildNodes(parent) {
     }
 }
 
-
-
-
-
 function loadInputValue() {
     removeChildNodes(divContainer)
     
     const inputValue = document.getElementById('gridValue').value;
     createDivsForGrid(inputValue)
     
-   
 }
 
 loadButton.addEventListener('click', loadInputValue);
